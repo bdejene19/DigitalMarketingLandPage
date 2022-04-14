30ebc0b9f230660347c29a56f7892bb99980cbd1
Bemnet Dejene <bdejene19@gmail.com>
2022-04-13 20:22:33 -0400

    maintained outdated images

--- src/components/OurPromise.js
+++ src/components/OurPromise.js
@@ -1,52 +1,96 @@
-import React from 'react';
-import {Typography, Button} from '@material-ui/core';
-import {OffersWrapper, AvailableOffer, OurPromiseWrapper} from './styledComponents';
+import React from "react";
+import { Typography, Button } from "@material-ui/core";
+import {
+  OffersWrapper,
+  AvailableOffer,
+  OurPromiseWrapper,
+} from "./styledComponents";
 
 export default function OurPromise() {
-    return (
-        <section>
-            <article>
-              <OffersWrapper inputImage="https://www.mainstreetmediasavvy.com/wp-content/uploads/2020/01/business-Marketing.jpg">
-                <AvailableOffer className='snap-location'> 
-                  <Typography variant='h4'><b>Our Promise</b></Typography>
-                  <OurPromiseWrapper>
-                    <div>
-                      <img src="/guarenteeRibbon.png" alt='blue ribbon'></img>
-                      <div className='promiseText'>
-                        <Typography variant='subtitle1'><b>NLM Guarentee</b></Typography>
-                        <Typography variant='subtitle2'>We work for you, and you only. We commit to one client, per industry, per...</Typography>
-                      </div>
-                    </div>
+  return (
+    <section>
+      <article>
+        <OffersWrapper inputImage="https://www.mainstreetmediasavvy.com/wp-content/uploads/2020/01/business-Marketing.jpg">
+          <AvailableOffer className="snap-location">
+            <Typography variant="h4">
+              <b>Our Promise</b>
+            </Typography>
+            <OurPromiseWrapper>
+              <div>
+                <img
+                  src="https://listimg.pinclipart.com/picdir/s/2-20126_sky-blue-ribbon-blue-ribbon-clipart-png-download.png"
+                  alt="blue ribbon"
+                ></img>
+                <div className="promiseText">
+                  <Typography variant="subtitle1">
+                    <b>NLM Guarentee</b>
+                  </Typography>
+                  <Typography variant="subtitle2">
+                    We work for you, and you only. We commit to one client, per
+                    industry, per...
+                  </Typography>
+                </div>
+              </div>
 
-                    <div>
-                      <img src="guarenteeRibbon.png" alt='guarentee ribbon'></img>
-                      <div className='promiseText'>
-                        <Typography variant='subtitle1'><b>PPC Guarentee</b></Typography>
-                        <Typography variant='subtitle2'>If you don’t receive at least a 3-1 Return on Investment in Months...</Typography>
-                      </div>
-                    </div>
+              <div>
+                <img
+                  src="http://miniclips.phillipmartin.info/ribbon/ribbon_yellow.png"
+                  alt="guarentee ribbon"
+                ></img>
+                <div className="promiseText">
+                  <Typography variant="subtitle1">
+                    <b>PPC Guarentee</b>
+                  </Typography>
+                  <Typography variant="subtitle2">
+                    If you don’t receive at least a 3-1 Return on Investment in
+                    Months...
+                  </Typography>
+                </div>
+              </div>
 
-                    <div>
-                      <img src="/guarenteeRibbon.png" alt='blue ribbon'></img>
-                      <div className='promiseText'>
-                        <Typography variant='subtitle1'><b>SEO Guarentee</b></Typography>
-                        <Typography variant='subtitle2'>We will rank you on the first page of the major search engines for at...</Typography>
-                      </div>
-                    </div>
+              <div>
+                <img
+                  src="https://listimg.pinclipart.com/picdir/s/2-20126_sky-blue-ribbon-blue-ribbon-clipart-png-download.pnghttps://listimg.pinclipart.com/picdir/s/2-20126_sky-blue-ribbon-blue-ribbon-clipart-png-download.png"
+                  alt="blue ribbon"
+                ></img>
+                <div className="promiseText">
+                  <Typography variant="subtitle1">
+                    <b>SEO Guarentee</b>
+                  </Typography>
+                  <Typography variant="subtitle2">
+                    We will rank you on the first page of the major search
+                    engines for at...
+                  </Typography>
+                </div>
+              </div>
 
-                    <div>
-                      <img src="https://cdn0.iconfinder.com/data/icons/business-687/32/icons-19-2-512.png" alt='data'></img>
-                      <div className='promiseText'>
-                        <Typography variant='subtitle1'><b>Datacentric Management</b></Typography>
-                        <Typography variant='subtitle2'>Our team eliminates guesswork with proprietary tracking and...</Typography>
-                      </div>
-                    </div>
-                  </OurPromiseWrapper>
-                  <Button variant='outlined' color='primary' size='medium' style={{gridRow: 4}}>Schedule A Call!</Button>
-
-                </AvailableOffer>
-              </OffersWrapper>
-            </article>
-          </section>
-    )
+              <div>
+                <img
+                  src="https://cdn0.iconfinder.com/data/icons/business-687/32/icons-19-2-512.png"
+                  alt="data"
+                ></img>
+                <div className="promiseText">
+                  <Typography variant="subtitle1">
+                    <b>Datacentric Management</b>
+                  </Typography>
+                  <Typography variant="subtitle2">
+                    Our team eliminates guesswork with proprietary tracking
+                    and...
+                  </Typography>
+                </div>
+              </div>
+            </OurPromiseWrapper>
+            <Button
+              variant="outlined"
+              color="primary"
+              size="medium"
+              style={{ gridRow: 4 }}
+            >
+              Schedule A Call!
+            </Button>
+          </AvailableOffer>
+        </OffersWrapper>
+      </article>
+    </section>
+  );
 }
--- src/components/SEOSection.js
+++ src/components/SEOSection.js
@@ -1,31 +1,48 @@
-import React from 'react';
-import {Typography, Button} from '@material-ui/core';
-import {OffersWrapper, AvailableOffer} from './styledComponents';
+import React from "react";
+import { Typography, Button } from "@material-ui/core";
+import { OffersWrapper, AvailableOffer } from "./styledComponents";
 
 export default function SEOSection() {
-    return (   
-        <section>
-            <article>
-            <OffersWrapper inputImage="https://www.explainervideoagency.com/blog/wp-content/uploads/2017/01/Main-Header-5-Advanced-You-Tube-Video-Optimization-Tactics.jpg">
-                <AvailableOffer className='snap-location'>
-                <div>
-                    <Typography variant='h4'><b>Search Engine Optimization</b></Typography>
-                </div>
+  return (
+    <section>
+      <article>
+        <OffersWrapper inputImage="https://media.istockphoto.com/vectors/23web-search-technology-search-engine-seo-data-finding-search-bar-vector-id1192865248?k=20&m=1192865248&s=612x612&w=0&h=Cqy79F63VcxQbWOy3mxstkEhVefjUfsbpafs-0wVtEY=">
+          <AvailableOffer className="snap-location">
+            <div>
+              <Typography variant="h4">
+                <b>Search Engine Optimization</b>
+              </Typography>
+            </div>
 
-                <div className='pText'>
-                    <Typography variant='body2'>SEO, or Search Engine Optimization, is truly an art form. If executed properly, SEO can be one of the best forms of marketing you can do to promote your website, while being the most cost effective.</Typography>
-                </div>
-                
-                <div className='awards'>
-                    <img src="https://websiterelease.com/wp-content/uploads/2016/11/Search-Engine-Optimization-SEO-banner.jpg" alt="SEO practices"></img>
-                    <img src="https://www.boostability.com/wp-content/uploads/2015/03/10BestSEO-Logo-419x500.png" alt="Top 10 SEO"></img>
-                    <img src="https://www.seoworks.co.uk/wp-content/uploads/2018/06/Google-Partner-Specialist-Badge.png" alt="Google Partners"></img>
-                </div>
-                <Button variant='outlined' color='primary' size='medium'>Learn More</Button>
+            <div className="pText">
+              <Typography variant="body2">
+                SEO, or Search Engine Optimization, is truly an art form. If
+                executed properly, SEO can be one of the best forms of marketing
+                you can do to promote your website, while being the most cost
+                effective.
+              </Typography>
+            </div>
 
-                </AvailableOffer>
-            </OffersWrapper>
-            </article>
-        </section>
-    )
+            <div className="awards">
+              <img
+                src="https://websiterelease.com/wp-content/uploads/2016/11/Search-Engine-Optimization-SEO-banner.jpg"
+                alt="SEO practices"
+              ></img>
+              <img
+                src="https://www.boostability.com/wp-content/uploads/2015/03/10BestSEO-Logo-419x500.png"
+                alt="Top 10 SEO"
+              ></img>
+              <img
+                src="https://www.seoworks.co.uk/wp-content/uploads/2018/06/Google-Partner-Specialist-Badge.png"
+                alt="Google Partners"
+              ></img>
+            </div>
+            <Button variant="outlined" color="primary" size="medium">
+              Learn More
+            </Button>
+          </AvailableOffer>
+        </OffersWrapper>
+      </article>
+    </section>
+  );
 }
